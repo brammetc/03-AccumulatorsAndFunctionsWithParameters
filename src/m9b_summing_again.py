befcf113d774c342bab9501794fcf89eae0794c9
brammetc <brammetc@rose-hulman.edu>
2018-12-02 18:50:36 -0500

    DONE

--- src/m9b_summing_again.py
+++ src/m9b_summing_again.py
@@ -4,8 +4,8 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Tanner Brammeier.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -17,7 +17,7 @@ def main():
 def run_test_sum_powers():
     """ Tests the   sum_powers   function. """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # DONE: 2. Implement this function.
     #   It TESTS the  sum_powers  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -29,9 +29,32 @@ def run_test_sum_powers():
     print('Testing the   sum_powers   function:')
     print('--------------------------------------------------')
 
+    # Test 1:
+    expected = 3.80826
+    answer = sum_powers(5, -0.3)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+
+    # Test 2:
+    expected = 144.45655
+    answer = sum_powers(100, 0.1)
+    print('Test 2 expected:', expected)
+    print('       actual:  ', answer)
+
+    # Test 3:
+    expected = 15
+    answer = sum_powers(5, 1)
+    print('Test 3 expected:', expected)
+    print('       actual:  ', answer)
+
 
 def sum_powers(n, p):
-    """
+    total = 0
+    for k in range(n):
+        total = total + ((k + 1) ** p)
+    return total
+
+"""
     What comes in:  A non-negative integer n
                     and a number p.
     What goes out:  The sum   1**p + 2**p + 3**p + ... + n**p
@@ -41,9 +64,9 @@ def sum_powers(n, p):
     Examples:
       -- sum_powers(5, -0.3) returns about 3.80826
       -- sum_powers(100, 0.1) returns about 144.45655
-    """
+"""
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers  to GENERATE
@@ -54,7 +77,7 @@ def sum_powers(n, p):
 def run_test_sum_powers_in_range():
     """ Tests the   sum_powers_in_range   function. """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_powers_in_range  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -66,9 +89,32 @@ def run_test_sum_powers_in_range():
     print('Testing the   sum_powers_in_range   function:')
     print('--------------------------------------------------')
 
+    # Test 1:
+    expected = 145.55928
+    answer = sum_powers_in_range(3, 100, 0.1)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+
+    # Test 2:
+    expected = 4424
+    answer = sum_powers_in_range(2, 4, 5)
+    print('Test 2 expected:', expected)
+    print('       actual:  ', answer)
+
+    # Test 3:
+    expected = 12200
+    answer = sum_powers_in_range(2, 5, 5)
+    print('Test 3 expected:', expected)
+    print('       actual:  ', answer)
+
 
 def sum_powers_in_range(m, n, p):
-    """
+    total = 0
+    for k in range(n):
+        total = total + ((m + k) ** p)
+    return total
+
+"""
     What comes in:  Non-negative integers m and n, with n >= m,
                     and a number p.
     What goes out:  the sum
@@ -78,7 +124,7 @@ def sum_powers_in_range(m, n, p):
     Side effects:  None.
     Example:
       -- sum_powers_in_range(3, 100, 0.1) returns about 142.384776
-    """
+"""
     # -------------------------------------------------------------------------
     # TODO: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
